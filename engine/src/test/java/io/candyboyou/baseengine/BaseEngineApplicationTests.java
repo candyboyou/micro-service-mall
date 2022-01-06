6b75bf2034bb6c08869a59af22c39de5056f4bbc
candy_boy <balaraking@gmail.com>
2022-01-07 00:27:24 +0800

    初步搭建框架

---
+++ engine/src/test/java/io/candyboyou/baseengine/BaseEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.candyboyou.baseengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gateway/src/test/java/io/candyboyou/gateway/GateWayTests.java
@@ -0,0 +1,13 @@
+package io.candyboyou.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
