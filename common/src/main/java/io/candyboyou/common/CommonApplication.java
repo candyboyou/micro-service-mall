38ab54ec52f483727853dff6936c54eae188be3f
candy_boy <balaraking@gmail.com>
2022-02-08 00:41:05 +0800

    解决版本匹配问题

--- common/src/main/java/io/candyboyou/common/CommonApplication.java
+++ common/src/main/java/io/candyboyou/common/CommonApplication.java
@@ -2,12 +2,35 @@ package io.candyboyou.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CommonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonApplication.class, args);
+
+		doPost();
+	}
+
+	public static void doPost() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+		map.add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E217 MicroMessenger/6.8.0(0x16080000) NetType/WIFI Language/en Branch/Br_trunk MiniProgramEnv/Mac");
+		map.add("Referer", "https://servicewechat.com/wx17ea87763491620f/1427/page-frame.html");
+
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.postForEntity( "https://app.m.mi.com/v2/cate/index", request , String.class );
 	}
 
 }
